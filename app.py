a67fa67451657d0aa9b8a03c2baa298345b9d3f1
CaioCarnelos <caio.emidio@gmail.com>
2020-09-24 19:45:56 +0100

    fix

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://leprechaum-bot.herokuapp.com/' + TOKEN)
+    SITE = config('SITE')
+    updater.bot.setWebhook(SITE + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
