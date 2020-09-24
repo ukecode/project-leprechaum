1b70563403915d3c1ea9bdbd8f29b1bf66e25b7e
CaioCarnelos <caio.emidio@gmail.com>
2020-09-24 19:45:08 +0100

    input webhook

--- app.py
+++ app.py
@@ -56,6 +56,11 @@ def main():
     # Start the Bot
     updater.start_polling()
 
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://leprechaum-bot.herokuapp.com/' + TOKEN)
+
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
